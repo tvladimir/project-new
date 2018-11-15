9e595837c78589e81be9f806440c4676e652c146
Vladimir <trofimyuk@gmail.com>
2018-11-15 14:50:55 +0200

    Initial 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-new';
+
+  img_path = ['/assets/images/UNADJUSTEDNONRAW_thumb_3ea.jpg',
+              '/assets/images/UNADJUSTEDNONRAW_thumb_405.jpg',
+              '/assets/images/UNADJUSTEDNONRAW_thumb_413.jpg']
+  public currentImagePlace: number;
+  public imagePath: string;
+
+  constructor(){
+    this.currentImagePlace = 0;
+    this.imagePath = this.img_path[this.currentImagePlace];
+  }
+
+  showAlert(typeButton: string){
+    const typeText = typeButton === 'left' ? 'first' : 'last';
+
+    const message = "This is a " + typeText + "last Picture";
+
+    console.log(message);
+
+  }
+
+  onSliderButtonClick(typeButton: string) {
+    switch (typeButton) {
+
+      case 'left':
+        if (this.currentImagePlace > 0){
+          this.currentImagePlace--;
+          this.imagePath = this.img_path[this.currentImagePlace];
+        }else{
+            this.showAlert(typeButton);
+        }
+        break;
+
+      case 'right':
+        if (this.currentImagePlace < this.img_path.length - 1){
+          this.currentImagePlace++;
+          this.imagePath = this.img_path[this.currentImagePlace];
+        }else{
+            this.showAlert(typeButton);
+        }
+      break;
+    
+      default:
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,54 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
